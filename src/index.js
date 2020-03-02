69005088e594beb09b7435ddeea5821e42f2eb94
Santiago Rhenals <santiagorhenals@gmail.com>
2020-03-02 19:09:55 +0100

    Adding comments

--- src/index.js
+++ src/index.js
@@ -139,7 +139,7 @@ function Square(props) {
       [2, 4, 6],
     ];
     for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
+      const [a, b, c] = lines[i]; //* Decomposing the lines into 3 elements
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
         return squares[a];
       }
